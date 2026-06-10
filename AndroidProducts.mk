688c7b5f4c493d2f74311a5c560e345b85537d42
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2026-06-10 13:26:32 +0500

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/infinty_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    infinty_spes-user \
+    infinty_spes-userdebug \
+    infinty_spes-eng
